093401eddf96945460c41943021dccd02fb5f687
lilimik <kazakovlim@inbox.ru>
2021-08-01 12:53:00 +0300

    add swagger with drf

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(http_method_names=['GET'])
+def main_api_view(request):
+    """Метод проверка api"""
+    return Response({'status': 'ok'})
